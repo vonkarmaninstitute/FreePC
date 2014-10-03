6e74c5712c9386e315b0ae9c92c1cb324cf46863
lspitaels <spitaels@vki.ac.be>
2014-10-03 15:02:05 +0200

    remove key

--- server/login_system/settings.py
+++ server/login_system/settings.py
@@ -31,7 +31,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-hmrk&(q5c(@#j6*=f3hglsnzs*y5sx7%&psh1((vdlbbybuj$'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
